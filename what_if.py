715f5451d751132b21a944bece73332d0242eb5d
Pail Hung <100525885+PepperPotatoPail@users.noreply.github.com>
2022-04-22 00:17:19 +0800

    Update what_if.py

--- what_if.py
+++ what_if.py
@@ -7,10 +7,10 @@ Created on Sun Apr 17 18:27:09 2022
 """
 import yfinance as yf
 import pandas as pd
+pd.options.mode.chained_assignment = None
 import matplotlib.pyplot as plt
 import textcleaner as tc
 import numpy as np
-import math
 import os
 from wordcloud import WordCloud
 
@@ -22,13 +22,13 @@ if ticker_required == '':
 
 start_date = input('start(YYYY-MM-DD):')
 if start_date == '':
-    start_date = '2019-01-01'
-    print('No input, default 2019-01-01')
+    start_date = '2020-01-01'
+    print('No input, default 2020-01-01')
 
 end_date = input('end(YYYY-MM-DD):')
 if end_date == '':
-    end_date = '2019-12-31'
-    print('No input, default 2019-12-31')
+    end_date = '2020-12-31'
+    print('No input, default 2020-12-31')
 
 # download stock price history from yahoo finance
 oneday_int_df = yf.download(ticker_required,
@@ -43,22 +43,28 @@ rawdata = pd.read_csv(export_path + os.sep + 'Updated_Data.csv')
 
 core_data_df = rawdata[['date','ticker','Updated Score']]
 core_data_df.dropna(inplace=True) #drop nan values
+core_data_df = core_data_df.copy()
 core_data_df['Date'] = core_data_df['date'].astype(str).str[:10] #keep date only, no need time
+core_data_df = core_data_df.copy()
 core_data_df = core_data_df[core_data_df.columns[1:4]]
 
 # aggregate the scores of same ticker on same day into average score
 aggregation_functions = {'Updated Score': 'mean'}
 integrated_sentiment_df = core_data_df.groupby(['Date','ticker']).aggregate(aggregation_functions).reset_index()
+integrated_sentiment_df = integrated_sentiment_df.copy()
 # select the data with specific ticker collected only
 sentiment_merge_df = integrated_sentiment_df[integrated_sentiment_df['ticker']== ticker_required]
 # drop the ticker column as long as all data comes within same column
 sentiment_merge_df.drop('ticker', inplace=True, axis=1)
+sentiment_merge_df = sentiment_merge_df.copy()
 
 # Merge 2 dataframes
 process_df = oneday_int_df.merge(sentiment_merge_df, how='left', on='Date')
 # fillna to 0
+process_df = process_df.copy()
 process_df['Updated Score'] = process_df['Updated Score'].fillna(0)
 # change Date column to datetime format
+process_df = process_df.copy()
 process_df['Date'] = process_df['Date'].astype('datetime64[ns]')
 
 # Export paths
@@ -92,22 +98,6 @@ def buy_sell_WSB(signal):
     for i in range(0, len(signal)):
 
         if (  # trading policies
-                process_df['Updated Score'][i] > 0 
-        ):
-            Sell.append(np.nan)  # buy, not sell
-            if flag != 1:  # previous action not buy
-                buy.append(process_df['close'][i])  # mark item
-                flag = 1  # mark action as buy
-                # collect data for analysis later
-                WSB_result.append({
-                    'date': process_df['Date'][i],
-                    'action': 'buy',
-                    'price': process_df['close'][i]
-                })
-            else:  # previous action is buy
-                buy.append(np.nan)  # pass
-
-        elif (  # trading policies
                 process_df['Updated Score'][i] < 0 
         ):
             buy.append(np.nan)  # sell, not buy
@@ -122,6 +112,22 @@ def buy_sell_WSB(signal):
                 })
             else:  # previous action is sell
                 Sell.append(np.nan)  # pass
+
+        elif (  # trading policies
+                    process_df['Updated Score'][i] > 0 
+            ):
+                Sell.append(np.nan)  # buy, not sell
+                if flag != 1:  # previous action not buy
+                    buy.append(process_df['close'][i])  # mark item
+                    flag = 1  # mark action as buy
+                    # collect data for analysis later
+                    WSB_result.append({
+                        'date': process_df['Date'][i],
+                        'action': 'buy',
+                        'price': process_df['close'][i]
+                    })
+                else:  # previous action is buy
+                    buy.append(np.nan)  # pass
         else:  # not satisfied the trading policies
             buy.append(np.nan)  # both do nothing
             Sell.append(np.nan)
@@ -132,11 +138,18 @@ def buy_sell_WSB(signal):
 WSB_trade = buy_sell_WSB(process_df)
 
 # merge the sell and buy signals into the df
+process_df = process_df.copy()
 process_df['WSB_buy_signal'] = WSB_trade[0]
+process_df = process_df.copy()
 process_df['WSB_sell_signal'] = WSB_trade[1]
+process_df = process_df.copy()
+
+
+print('\n')
+print('Trading Timespots: ')
 
 # plot the figure displaying the trading timespot
-plt.figure(figsize=(12.2, 4.5))
+plt.figure(figsize=(8, 4.5))
 plt.scatter(process_df['Date'], process_df['WSB_sell_signal'],
             label='sell', marker='v', alpha=1,
             color='red')
@@ -148,9 +161,79 @@ plt.title('WSB Trading timespots' + " for " + ticker_required )
 plt.legend(loc='upper left')
 WSB_trading_expt = str(ticker_required) + ' WSB Trading timespots ' + str(
     start_date) + ' to ' + str(end_date) + str('.png')
-plt.savefig(os.path.join(mypath, WSB_trading_expt))
+plt.savefig(os.path.join(mypath, WSB_trading_expt), dpi=240)
+plt.gcf().set_dpi(80)
+plt.show()
 
 process_df['Date']= process_df['Date'].astype('datetime64[ns]')
+
+stronghold_return = float(oneday_int_df.tail(1)['close']) - float(
+    oneday_int_df.head(1)['close'])
+
+# wordcloud and frequency chart
+
+def wordcloud_generator(target_string, path=mypath):
+    cloud = WordCloud(collocations=False,
+                      background_color="white", #make a wordcloud
+                      width=1920,
+                      height=1080,
+                      margin=2).generate(target_string)
+    fullname = str(str(start_date) + ' to ' + str(end_date) + ' wordcloud.png')
+    cloud.to_file(os.path.join(path, fullname))
+    plt.figure(figsize=(8,4.5))
+    plt.imshow(cloud, interpolation='bilinear')
+    plt.gcf().set_dpi(80)
+    plt.axis("off")
+    plt.show()
+    return
+
+def string_word_count(target_string):
+    doc = tc.document(target_string) #use textcleaner to progress it
+    doc_dict = dict(doc.each_word_count) #textcleaner count
+    word_count_df = pd.DataFrame(list(doc_dict.items()), #transform dict to dataframe
+                                 columns=['ticker', 'frequency']).sort_values(by=['frequency'], ascending=False)
+    return word_count_df
+
+def freq_barchart(word_count_df, path=mypath): #make a frequency chart
+    chart = word_count_df.iloc[:10].plot.bar(x='ticker',
+                                             y='frequency',
+                                             rot=0,
+                                             figsize=(8, 4.5),
+                                             title=str('Top 10 Most Mentioned Tickers during ' + str(start_date) + ' to ' + str(end_date)))
+    fullname = str(str(start_date) + ' to ' + str(end_date) + ' freqchart.png')
+    plt.savefig(os.path.join(path, fullname), dpi=240)
+    plt.gcf().set_dpi(80)
+    plt.show()
+    return chart
+
+frequency_df = core_data_df.copy()
+# change Date column from string format to datetime format
+frequency_df.index = frequency_df['Date'].astype('datetime64[ns]')
+# filter the dataframe with specific timerange designated
+frequency_df = frequency_df.loc[start_date:end_date]
+
+# merge the dataframe column to string
+ticker_str=(" ").join(frequency_df['ticker'].tolist())
+print('\n')
+print('WordCloud: ')
+
+wordcloud_generator(ticker_str,mypath)
+ticker_count = string_word_count(ticker_str)
+print('\n')
+print('Frequency Chart: ')
+
+freq_barchart(ticker_count,mypath)
+
+print('\n')
+print('Done! Output in folder: ' + str(folder))
+
+print('\n' + stronghold_exp + '\n')
+print('return is ' + str(stronghold_return) + '\n')
+
+
+
+'''
+## Codes that are not used now ##
 
 # analysis the data collected from func_result
 def result_summary(signals):
@@ -196,12 +279,8 @@ def result_summary(signals):
 
     return export_str
 
-
 # get summary
 WSB_summary = result_summary(WSB_result)
-
-stronghold_return = float(oneday_int_df.tail(1)['close']) - float(
-    oneday_int_df.head(1)['close'])
 
 # export txt analysis
 txtname = str(ticker_required) + ' ' + str(start_date) + ' to ' + str(
@@ -220,55 +299,6 @@ f.write("WSB Result" + '\n' + WSB_summary + '\n')
 f.write('\n')
 f.close()
 
-
-# wordcloud and frequency chart
-
-def wordcloud_generator(target_string, path=mypath):
-    cloud = WordCloud(collocations=False,
-                      background_color="white", #make a wordcloud
-                      width=1920,
-                      height=1080,
-                      margin=2).generate(target_string)
-    fullname = str(str(start_date) + ' to ' + str(end_date) + ' wordcloud.png')
-    cloud.to_file(os.path.join(path, fullname))
-    plt.imshow(cloud, interpolation='bilinear')
-    plt.axis("off")
-    plt.show()
-    return
-
-def string_word_count(target_string):
-    doc = tc.document(target_string) #use textcleaner to progress it
-    doc_dict = dict(doc.each_word_count) #textcleaner count
-    word_count_df = pd.DataFrame(list(doc_dict.items()), #transform dict to dataframe
-                                 columns=['ticker', 'frequency']).sort_values(by=['frequency'], ascending=False)
-    return word_count_df
-
-def freq_barchart(word_count_df, path=mypath): #make a frequency chart
-    chart = word_count_df.iloc[:10].plot.bar(x='ticker',
-                                             y='frequency',
-                                             rot=0,
-                                             figsize=(15, 6),
-                                             title=str('Top 10 Most Mentioned Tickers during ' + str(start_date) + ' to ' + str(end_date)))
-    fullname = str(str(start_date) + ' to ' + str(end_date) + ' freqchart.png')
-    plt.savefig(os.path.join(path, fullname), dpi=300)
-    return chart
-
-frequency_df = core_data_df
-# change Date column from string format to datetime format
-frequency_df.index = frequency_df['Date'].astype('datetime64[ns]')
-# filter the dataframe with specific timerange designated
-frequency_df = frequency_df.loc[start_date:end_date]
-
-# merge the dataframe column to string
-ticker_str=(" ").join(frequency_df['ticker'].tolist())
-wordcloud_generator(ticker_str,mypath)
-ticker_count = string_word_count(ticker_str)
-freq_barchart(ticker_count,mypath)
-
-print('Done! Output in folder: ' + str(folder))
-
-print('\n' + stronghold_exp + '\n')
-print('return is ' + str(stronghold_return) + '\n')
 print(WSB_summary + '\n')
-
-#tickers = list(set(rawdata['ticker'].tolist()))
+tickers = list(set(rawdata['ticker'].tolist()))
+'''
